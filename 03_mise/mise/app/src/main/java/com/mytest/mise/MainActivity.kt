b9e2470536e2f30900e2a49a37d454a036a50041
kwonhyeokgeun <xkhg0611x@naver.com>
2023-08-02 14:44:45 +0900

    feature : 위치 권한

--- 03_mise/mise/app/src/main/java/com/mytest/mise/MainActivity.kt
+++ 03_mise/mise/app/src/main/java/com/mytest/mise/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.mytest.mise
 
 import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.Instrumentation.ActivityResult
+import android.content.DialogInterface
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.mytest.mise.databinding.ActivityMainBinding
@@ -15,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
     private val PERMISSION_REQUEST_CODE = 100 //어떤 리퀘스트인지 확인을 위한 id개념
-    private lateinit var getGPSPerssionLauncher : ActivityResult
+    private lateinit var getGPSPermissionLauncher : ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         //binding 객체 받기
@@ -27,7 +34,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun checkAllPermissions(){
-        if(!isLocationServiceAvailable()){ //위치서비스가 꺼져있다면
+        if(!isLocationServicesAvailable()){ //위치서비스가 꺼져있다면
             showDialogForLocationServiceSetting() //위치서비스 켜기 요청
         }else{
             isRuntimePermissionGranted() //위치관련 런타임 권한 설정되었는가
@@ -35,7 +42,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     //인터넷기반 위치나 gps기반 위치 둘중하나만 켜져있으면 true
-    private fun isLocationServiceAvailable():Boolean{
+    private fun isLocationServicesAvailable():Boolean{
         val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
         return (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
     }
@@ -72,13 +79,41 @@ class MainActivity : AppCompatActivity() {
             }else{
                 //권한이 거부함을 알리고 앱종료
                 Toast.makeText(this, "권한이 거부되어 앱이 종료됩니다.", Toast.LENGTH_LONG).show()
-                finish()
+                //finish()
             }
         }
     }
 
-    private fun showDialogForLocationServiceSetting(){
+    //권한이 있다면 위치 on시킴
+    private fun showDialogForLocationServiceSetting() {
+        //먼저 ActivityResultLauncher를 설정해줍니다. 이 런처를 이용하여 결과 값을 리턴해야하는 인텐트를 실행할 수 있습니다.
+        getGPSPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                if (isLocationServicesAvailable()) {
+                    isRuntimePermissionGranted()
+                } else { //위치 서비스가 허용되지 않았다면 앱을 종료합니다.
+                    Toast.makeText(this@MainActivity, "위치 서비스를 사용할 수 없습니다.", Toast.LENGTH_LONG).show()
+                    finish()
+                }
+            }
+        }
 
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage("위치 서비스가 꺼져있습니다. 설정해야 앱을 사용할 수 있습니다.")
+        builder.setCancelable(true)
+        builder.setPositiveButton("설정", DialogInterface.OnClickListener { dialog, id ->
+            val callGPSSettingIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            getGPSPermissionLauncher.launch(callGPSSettingIntent)
+        })
+        builder.setNegativeButton("취소", DialogInterface.OnClickListener { dialog, id ->
+            dialog.cancel()
+            Toast.makeText(this@MainActivity, "기기에서 위치서비스(GPS) 설정 후 사용해주세요.", Toast.LENGTH_SHORT).show()
+            finish()
+        })
+        builder.create().show()
     }
 
 
